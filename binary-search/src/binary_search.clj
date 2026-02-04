9ef37e28cdf465948c7ea4be384d848d98cebc97
gw2509 <265750+gw2509@users.noreply.github.com>
2026-02-04 12:52:41 +0000

    Exercise complete.

---
+++ binary-search/src/binary_search.clj
@@ -0,0 +1,18 @@
+(ns binary-search)
+
+
+(defn search-for
+  "Returns the index of num in coll, or -1 if num is not found."
+  [num coll]
+  (loop [l 0
+         h (dec (count coll))]
+    (if (> l h)
+      -1
+      (let [m (+ l (quot (- h l) 2))
+            v (nth coll m)]
+        (cond
+          (= v num) m
+          (> v num) (recur l (dec m))
+          :else (recur (inc m) h))))))
+
+
